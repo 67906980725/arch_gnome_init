d30f6ed3e6173e5aa25c189cf28bd38f4e6de8b1
v <zh1024@zohomail.cn>
2023-03-10 21:04:01 +0800

    update

--- recorder.sh
+++ recorder.sh
@@ -6,7 +6,7 @@ source ./install.sh
 install flameshot xdg-desktop-portal xdg-desktop-portal-wlr grim 
 
 # obs必要设置: 
-#    `右击`-`变换`-`拉伸到全屏`; 
+#    `来源或预览上右击`-`变换`-`拉伸到全屏`; 
 #    `文件-设置-输出-输出模式-高级`; 
 #    `输出-录像-格式` 设为 `mp4` (便于分享时在线预览)
 install obs-studio
