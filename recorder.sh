dfdd8a4a35a62162ee2f8d6d6cc9e357331ea407
v <zh1024@zohomail.cn>
2023-03-10 21:01:28 +0800

    update

--- recorder.sh
+++ recorder.sh
@@ -10,3 +10,5 @@ install flameshot xdg-desktop-portal xdg-desktop-portal-wlr grim
 #    `文件-设置-输出-输出模式-高级`; 
 #    `输出-录像-格式` 设为 `mp4` (便于分享时在线预览)
 install obs-studio
+#  设置中文不口口口
+install adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts adobe-source-han-mono-cn-fonts
