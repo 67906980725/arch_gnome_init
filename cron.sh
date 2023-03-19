7733dc75c2a697b785a821906e2f32cd322f3fd2
v <zh1024@zohomail.cn>
2023-03-19 19:31:41 +0800

    update

--- cron.sh
+++ cron.sh
@@ -5,6 +5,7 @@ source ./default_path.sh
 
 
 cp_conf_home ".local/bin/sunset_sunrise" # 需要到.local/bin/sunset_sunrise中改地区
+$HOME/.local/bin/sunset_sunrise init
 
 cp_conf_home ".local/bin/cron"
 sh ./app.sh desk "$BIN_PATH/cron" icron
